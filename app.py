9edf753af3c79c7a4a61813c198ef899a23cd99b
Sunny <rohitnikhandia1@gmail.com>
2023-02-06 11:58:23 +0530

    Add model changes and migrations, make sure migrations run before app starts.

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ from resources.tag import blp as TagBlueprint
 from resources.user import blp as UserBlueprint
 from flask_jwt_extended import JWTManager
 from blocklist import BLOCKLIST
+from dotenv import load_dotenv
 
 def create_app(db_url=None):
     app = Flask(__name__) #creates the flask app
+    load_dotenv()
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"  # standard for api documentation
--- migrations/versions/731328fa48e6_.py
+++ migrations/versions/731328fa48e6_.py
@@ -21,7 +21,7 @@ def upgrade():
     op.create_table('Users',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('username', sa.String(length=80), nullable=False),
-    sa.Column('password', sa.String(), nullable=False),
+    sa.Column('password', sa.String(length=256), nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('username')
     )
